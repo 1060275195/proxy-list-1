ed888ba94812bad2c6842c73f870178e7c42cb4b
Phil Zona <phil.b.zona@gmail.com>
2018-08-24 10:21:01 -0500

    Add ability to configure number of proxies in command

--- get_list.py
+++ get_list.py
@@ -1,3 +1,4 @@
+import sys
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -11,18 +12,37 @@ soup = BeautifulSoup(page, 'html.parser')
 proxy_table = soup.find('table')
 proxy_row = soup.find_all('tr')
 
-outFile = open('output.txt', 'a')
 
-# Configure the number of hops you want here.
-# Recommend at least 5, but more than 10-15 will be super slow
-number_of_rows = 10
+def generate_proxy_chain():
 
-for i in range(1, number_of_rows + 1):
-    currentProxyData = proxy_row[i].find_all('td')
-    proto = currentProxyData[4].string.lower()
-    ip_addr = currentProxyData[0].string
-    port = currentProxyData[1].string
-    currentRow = '{}  {}  {}'.format(proto, ip_addr, port)
-    outFile.write(currentRow + '\n')
+    if len(sys.argv) < 2:
+        # Configure the default number of hops you want here.
+        # Recommend at least 5, but more than 10-15 will be super slow
+        number_of_rows = 5
+    else:
+        if sys.argv[1].isdigit():
+            number_of_rows = sys.argv[1]
+        else:
+            err = """
+                Invalid number of proxies for the chain.
+                Please provide an integer."""
+            raise ValueError(err)
+            return -1
 
-outFile.close()
+    outFile = open('output.txt', 'w')
+
+    for i in range(1, int(number_of_rows) + 1):
+        currentProxyData = proxy_row[i].find_all('td')
+
+        proto = currentProxyData[4].string.lower()
+        ip_addr = currentProxyData[0].string
+        port = currentProxyData[1].string
+
+        currentRow = '{}  {}  {}'.format(proto, ip_addr, port)
+        outFile.write(currentRow + '\n')
+
+    outFile.close()
+    return 0
+
+
+generate_proxy_chain()
